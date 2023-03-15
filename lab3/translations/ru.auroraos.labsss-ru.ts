996899caff65b6c54f30b6271c6397fb020c04cd
unknown <bulgach09@gmail.com>
2023-03-15 17:02:25 +0300

    lab3 added

---
+++ lab3/translations/ru.auroraos.labsss-ru.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name>AnimSquares</name>
+    <message>
+        <location filename="../qml/pages/AnimSquares.qml" line="15"/>
+        <source>Анимация квадраты</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DefaultCoverPage</name>
+    <message>
+        <location filename="../qml/cover/DefaultCoverPage.qml" line="47"/>
+        <source>Application Template</source>
+        <translation>Шаблон приложения</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="51"/>
+        <source>ЛАБА</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="59"/>
+        <source>Статические квадраты</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="67"/>
+        <source>Анимация</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="76"/>
+        <source>Калькулятор</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>StaticSquares</name>
+    <message>
+        <location filename="../qml/pages/StaticSquares.qml" line="15"/>
+        <source>Квадраты</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StaticSquares.qml" line="54"/>
+        <source>Квадрат</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
